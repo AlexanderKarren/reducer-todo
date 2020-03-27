f7bcd97a718746d65ea81e00113ef41a59653209
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-03-27 14:49:10 -0600

    refreshed packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoList from './components/TodoList'
+import SortedTodoList from './components/SortedTodoList'
 import Settings from './components/Settings'
 import { ReactSVG } from 'react-svg'
 import { Switch, Route, useHistory } from 'react-router-dom'
@@ -19,6 +20,7 @@ export default function App() {
       <div className={settingsOpen ? "main-heading settings-open" :"main-heading"}><h1>Todo App 2</h1><ReactSVG src="gear.svg" onClick={toggleSettings}/></div>
       <Switch>
         <Route path="/settings"><Settings setSettingsOpen={setSettingsOpen}/></Route>
+        <Route path="/:tag"><SortedTodoList /></Route>
         <Route path="/"><TodoList /></Route>
       </Switch>
     </div>
